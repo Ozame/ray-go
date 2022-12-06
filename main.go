663687276afc042ed81036d99136b195ec86762c
Samu Kumpulainen <onesamu@gmail.com>
2022-12-06 15:37:11 +0200

    Test plotting on projectiles

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	models.ShootCannon()
+	models.ShootCannonAndDraw()
 }
--- models/projectiles.go
+++ models/projectiles.go
@@ -1,6 +1,10 @@
 package models
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 type Projectile struct {
 	position Tuple // a point
@@ -22,10 +26,38 @@ func tick(env Environment, proj Projectile) Projectile {
 func ShootCannon() {
 	var p Projectile = Projectile{NewPoint(0, 1, 0), Normalize(NewVector(1, 1, 0))}
 	var e Environment = Environment{NewVector(0, -0.1, 0), NewVector(-0.01, 0, 0)}
-  println("Shooting the cannon")
+	println("Shooting the cannon")
 	for p.position.Y >= 1 {
 		p = tick(e, p)
-    fmt.Printf("Position: %v, Velocity: %v\n", p.position, p.velocity)
+		fmt.Printf("Position: %v, Velocity: %v\n", p.position, p.velocity)
 	}
-  println("Projectile landed")
+	println("Projectile landed")
+}
+
+func ShootCannonAndDraw() {
+	start := NewPoint(0, 1, 0)
+	velocity := Multiply(Normalize(NewVector(1, 1.8, 0)), 11.25)
+	p := Projectile{start, velocity}
+
+	gravity := NewVector(0, -0.1, 0)
+	wind := NewVector(-0.01, 0, 0)
+	e := Environment{gravity, wind}
+
+	canvas := NewCanvas(900, 550)
+
+	for p.position.Y >= -550 {
+		p = tick(e, p)
+		if p.position.X < 900 && 0 <= p.position.Y {
+			log.Printf("position is %v", p)
+			canvas.WritePixel(int64(p.position.X), 550-int64(p.position.Y), Color{1, 0, 0})
+		}
+	}
+
+	plot := canvas.ToPPM()
+
+	err := os.WriteFile("./../plot.ppm", []byte(plot), 0644)
+
+	if err != nil {
+		log.Fatal("File write failed")
+	}
 }
